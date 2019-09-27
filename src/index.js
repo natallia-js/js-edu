7e178898c520500fb39a13652690e72f3e91e5a8
Natallia <55149770+natallia-js@users.noreply.github.com>
2019-09-27 10:48:48 +0300

    feat implement a getTimeForEduction function

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
+
+        let time_needed = knowsProgramming ? 800 : 1300;
+
+        if (config.hasOwnProperty(focus)) {
+            return Math.ceil(time_needed/config[focus]);
+        }
+
       return 0;
   };
   
